0019237e3b86f91affe952defa3f250206dc99c9
gkc <gary.casey@gmail.com>
2022-05-23 12:34:01 +0100

    Tweaked READMEs

--- at_secondary_proxy/bin/main.dart
+++ at_secondary_proxy/bin/main.dart
@@ -3,9 +3,13 @@ import 'package:at_secondary_proxy/at_secondary_proxy.dart';
 
 Future<void> main(List<String> arguments) async {
   String rootHost = 'vip.ve.atsign.zone';
+  int proxyListenPort = 64000;
   if (arguments.isNotEmpty) {
     rootHost = arguments[0];
   }
-  var server = SecondaryProxyServer(64000, CacheableSecondaryAddressFinder(rootHost, 64));
+  if (arguments.length > 1) {
+    proxyListenPort = int.parse(arguments[1]);
+  }
+  var server = SecondaryProxyServer(proxyListenPort, CacheableSecondaryAddressFinder(rootHost, 64));
   server.startServing();
 }
