a50ae1987bea420afba523c8bcb1b0f4f7168bc3
Colin Constable <colin@constable.cc>
2025-05-15 18:33:31 -0700

    fix: Better arguments

--- packages/at_secondary_proxy/bin/main.dart
+++ packages/at_secondary_proxy/bin/main.dart
@@ -4,25 +4,29 @@ import 'dart:io';
 
 Future<void> main(List<String> arguments) async {
   String proxyUrl = 'vip.ve.atsign.zone:64';
+  String rootUrl = 'root.atsign.org:64';
   String rootHost = 'root.atsign.org';
   int proxyListenPort = 64;
+  int rootPort = 64;
   if (arguments.length == 2) {
     proxyUrl = arguments[0];
-    rootHost = arguments[1];
+    rootUrl = arguments[1];
   } else {
     error();
   }
   try {
-    proxyListenPort = int.parse(proxyUrl.split(';')[0]);
+    proxyListenPort = int.parse(proxyUrl.split(':')[1]);
+    rootHost = rootUrl.split(':')[0];
+    rootPort = int.parse(rootUrl.split(':')[1]);
   } catch (e) {
     error;
   }
-  var server = SecondaryProxyServer(proxyUrl, proxyListenPort, CacheableSecondaryAddressFinder(rootHost, 64));
+  var server = SecondaryProxyServer(proxyUrl, proxyListenPort, CacheableSecondaryAddressFinder(rootHost, rootPort));
   server.startServing();
 }
 
 void error() {
-  print('Usage: at_proxyserver <this proxies URL> <upstream atDirectory>');
+  print('Usage: at_proxyserver <this proxies URL> <upstream atDirectory URL>');
   print(' E.g. at_proxyserver vip.ve.atsign.zone:64 root.atsign.org:64');
   exit(1);
 }
