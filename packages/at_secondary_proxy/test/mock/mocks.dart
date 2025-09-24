528dd670915634c8d2e23cca73b32d41edfffc28
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-09-24 16:39:34 -0400

    feat(test): 2 secondary_connection_bridge_test.dart

--- packages/at_secondary_proxy/test/mock/mocks.dart
+++ packages/at_secondary_proxy/test/mock/mocks.dart
@@ -1,15 +1,49 @@
+import 'dart:async';
 import 'dart:io';
+import 'dart:typed_data';
+
 import 'package:at_lookup/at_lookup.dart';
 import 'package:at_secondary_proxy/src/secondary_connection_bridge.dart';
-import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
 
-@GenerateMocks(
-  [
-    SecureSocket,
-    SecureServerSocket,
-    SecondaryAddressFinder,
-    SecondaryConnectionBridge,
-  ],
-)
+// Lightweight hand-written mocks so we can control listen/done behavior in unit tests.
+class SecureSocketMock extends Mock implements SecureSocket {
+  final StreamController<Uint8List> _controller = StreamController<Uint8List>();
+  StreamController<Uint8List> get controller => _controller; // a way to add data to the stream manually, useful for testing
 
-void main() {}
+  // `done` is used for error handling
+  // this mirrors SecureSocket.done which is a Future that completes when the socket is closed
+  Future<void> _doneFuture = Future<void>.value();
+
+  void setDoneFuture(Future<void> future) {
+    _doneFuture = future;
+  }
+
+  @override
+  Future<void> get done => _doneFuture;
+
+  @override
+  StreamSubscription<Uint8List> listen(
+    void Function(Uint8List data)? onData, {
+    Function? onError,
+    void Function()? onDone,
+    bool? cancelOnError,
+  }) {
+    return _controller.stream.listen(
+      onData,
+      onError: onError,
+      onDone: onDone,
+      cancelOnError: cancelOnError,
+    );
+  }
+
+  Future<void> closeController() async {
+    await _controller.close();
+  }
+}
+
+class SecureServerSocketMock extends Mock implements SecureServerSocket {}
+
+class SecondaryAddressFinderMock extends Mock implements SecondaryAddressFinder {}
+
+class SecondaryConnectionBridgeMock extends Mock implements SecondaryConnectionBridge {}
--- packages/at_secondary_proxy/test/secondary_connection_bridge_test.dart
+++ packages/at_secondary_proxy/test/secondary_connection_bridge_test.dart
@@ -1,13 +1,62 @@
+import 'dart:async';
+import 'dart:typed_data';
+
+import 'package:at_secondary_proxy/at_secondary_proxy.dart';
+import 'package:mockito/mockito.dart';
 import 'package:test/test.dart';
 
+import 'package:at_secondary_proxy/src/secondary_connection_bridge.dart';
+
+import 'mock/mocks.dart';
+
+const dummyProxyUrl = 'proxy.example';
+
 void main() {
-  group('SecondaryConnectionBridge', () {
-    setUp(() {
-      // TODO
+  group('SecondaryConnectionBridge start state', () {
+    // create Mocks
+    final clientSocket = SecureSocketMock();
+    final addressFinder = SecondaryAddressFinderMock();
+    clientSocket.setDoneFuture(Future<void>.value());
+
+    final writes = <String>[];
+
+    // define how clientSocket responds to .write
+    when(clientSocket.write(any)).thenAnswer((invocation) {
+      writes.add(invocation.positionalArguments[0] as String);
+      return;
     });
 
-    tearDown(() {
-      // TODO
+    // create SecondaryConnectionBridge
+    SecondaryConnectionBridge(dummyProxyUrl, clientSocket, addressFinder);
+
+    test('SecondaryConnectionBridge sends "@" prompt upon construction', () async {
+      // in this test,
+      // we send a valid "from:<atSign>" command to the proxy server
+      // example: "from:@alice\n"
+      // we expect it to send back "@" prompt when the connection is established
+      // this indicates that the proxy server created a bridge for us to that atServer
+      expect(writes, contains('@'));
+    });
+
+    test('SecondaryConnectionBridge is in `opening` state and rejects non-`from:` commands', () async {
+      // in this test,
+      // we send a non-valid command to the proxy server
+      // we expect it to return the address of the proxy server back to us (Which is the same address we connected with anyways)
+
+      // Simulate sending an invalid command
+      final Uint8List invalidCommand = Uint8List.fromList('invalid_command\n'.codeUnits);
+      clientSocket.controller.add(invalidCommand);
+
+      // give some time for the async processing to complete
+      await Future.delayed(Duration(milliseconds: 100));
+
+      // the SecondaryConnectionBridge should respond with the proxyUrl
+      // because the command was invalid, so it is telling us to potentially reconnect via the proxyUrl
+      expect(writes, contains('$dummyProxyUrl\n'));
+    });
+
+    tearDownAll(() async {
+      await clientSocket.closeController();
     });
   });
 }
