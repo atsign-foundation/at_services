3f794f9086faeffe05ab0d705c2f95fda50c2dd2
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-09-24 13:11:28 -0400

    chore(mocks): remove custom mocks and rebuild

--- packages/at_secondary_proxy/test/mock/mocks.dart
+++ packages/at_secondary_proxy/test/mock/mocks.dart
@@ -1,7 +1,4 @@
-import 'dart:async';
 import 'dart:io';
-import 'dart:typed_data';
-
 import 'package:at_lookup/at_lookup.dart';
 import 'package:at_secondary_proxy/src/secondary_connection_bridge.dart';
 import 'package:mockito/annotations.dart';
@@ -12,12 +9,6 @@ import 'package:mockito/annotations.dart';
     SecureServerSocket,
     SecondaryAddressFinder,
     SecondaryConnectionBridge,
-  ],
-  customMocks: [
-    MockSpec<StreamSubscription<Uint8List>>(
-        as: #MockUint8ListStreamSubscription),
-    MockSpec<StreamSubscription<SecureSocket>>(
-        as: #MockSecureSocketStreamSubscription),
   ],
 )
 
