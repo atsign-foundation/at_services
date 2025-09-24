869fca63b102f07eb69b787f5a1534f3aeeadb72
JeremyTubongbanua <jeremy.tubongbanua@gmail.com>
2025-09-24 13:10:28 -0400

    feat: start `secondary_connection_bridge_test.dart`

---
+++ packages/at_secondary_proxy/test/secondary_connection_bridge_test.dart
@@ -0,0 +1,13 @@
+import 'package:test/test.dart';
+
+void main() {
+  group('SecondaryConnectionBridge', () {
+    setUp(() {
+      // TODO
+    });
+
+    tearDown(() {
+      // TODO
+    });
+  });
+}
